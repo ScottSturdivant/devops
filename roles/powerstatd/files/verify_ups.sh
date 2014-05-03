c2cccb2f4644ca9b8e699b0e5193eb6edbf96780
Scott Sturdivant <scott.sturdivant@gmail.com>
2014-05-02 19:53:56 -0600

    Remove pwrstat cronjob output on successes.

--- roles/powerstatd/files/verify_ups.sh
+++ roles/powerstatd/files/verify_ups.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-[ "`/usr/sbin/pwrstat -status | grep Test | tr -d '.' | awk '{print $3}'`" == "Passed" ] && echo "UPS is good." || echo "UPS FAILURE!"
+[ "`/usr/sbin/pwrstat -status | grep Test | tr -d '.' | awk '{print $3}'`" == "Passed" ] || echo "UPS FAILURE!, check 'sudo /usr/sbin/pwrstat -status'"
